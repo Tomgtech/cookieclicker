4c90fbd21835635a70525e0d6bf62b3567814def
Tomgtech <tom.gendall@yahoo.com>
2024-01-19 16:03:51 +0000

    even more cookies

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-console.log("Hello");
-
 let cookies = 0;
 const cookiebtn = document.getElementById("cookiebtn");
 const numberOfCookies = document.getElementById("numberOfCookies"); // Select the element by ID
 
 cookiebtn.addEventListener("click", function () {
   cookies++;
-  updateCookieCount(); // Call a function to update the displayed count
 });
 
 // Function to update the displayed count
 function updateCookieCount() {
-  numberOfCookies.textContent = cookies; // Update the text content with the current count
-  storeCookieCount();
+  document.getElementById("numberOfCookies").textContent = cookies; // Update the text content with the current count
 }
 
 // Start cookies count at zero
-setInterval(function () {
-  cookies = cookies + 1;
-  console.log("🍪", cookies);
-  updateCookieCount(); // Call the function to update the displayed count
-}, 1000);
-
-function storeCookieCount() {
-  localStorage.setItem("cookies", cookies);
+function playgame() {
+  getCookieCount();
+  setInterval(function () {
+    cookies++;
+    console.log("", cookies);
+    storeCookieCount();
+    updateCookieCount(); // Call the function to update the displayed count
+  }, 1000);
 }
 
-document.getElementById("resetButton");
-g.addEventListener("click", () => {
-  f();
-});
+// stores number of cookies to local storage
+function storeCookieCount() {
+  localStorage.setItem("numberOfCookies", cookies);
+}
+
+// retrieves number of cookies from local storage
+function getCookieCount() {
+  cookies = localStorage.getItem("numberOfCookies");
+}
+
+playgame();
+
+function reset() {
+  cookies = 0;
+  storeCookieCount();
+  playgame();
+}
